bd8dc82f7e471d79c08b8725c3c87419efb7d842
Sinchana H M <sinchu0211@gmail.com>
2026-09-07 18:56:47 +0530

    Update mux21_Dataflow_testbench.v

--- mux21_Dataflow_testbench.v
+++ mux21_Dataflow_testbench.v
@@ -4,7 +4,7 @@ module testbench;
   reg s;
   wire y;
 
-  // Instantiate the DUT
+  
   mux21 uut (
     .i(i),
     .s(s),
@@ -13,27 +13,27 @@ module testbench;
 
   initial begin
 
-    // Test 1
+   
     i = 2'b00; s = 0;
     #10;
     $display("i=%b, s=%b, y=%b", i, s, y);
 
-    // Test 2
+    
     i = 2'b01; s = 0;
     #10;
     $display("i=%b, s=%b, y=%b", i, s, y);
 
-    // Test 3
+    
     i = 2'b10; s = 0;
     #10;
     $display("i=%b, s=%b, y=%b", i, s, y);
 
-    // Test 4
+   
     i = 2'b11; s = 0;
     #10;
     $display("i=%b, s=%b, y=%b", i, s, y);
 
-    // Now test s = 1
+   
 
     i = 2'b00; s = 1;
     #10;
