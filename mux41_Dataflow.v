74130370ae272f6c54305654e21d7771cd7c8f3c
Sinchana H M <sinchu0211@gmail.com>
2026-09-07 19:44:00 +0530

    Create mux41_Dataflow.v

---
+++ mux41_Dataflow.v
@@ -0,0 +1,12 @@
+module mux41(
+    input  [3:0] i,
+    input  [1:0] s,
+    output y
+);
+
+assign y = (s == 2'b00) ? i[0] :
+           (s == 2'b01) ? i[1] :
+           (s == 2'b10) ? i[2] :
+                          i[3];
+
+endmodule
