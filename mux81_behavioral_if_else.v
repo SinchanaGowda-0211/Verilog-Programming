bb1215c13081eec6cdefc5461e2a6b00511d4138
Sinchana H M <sinchu0211@gmail.com>
2026-09-09 22:09:41 +0530

    Create mux81_behavioral_if_else.v

---
+++ mux81_behavioral_if_else.v
@@ -0,0 +1,28 @@
+module mux81(
+    input [7:0] i,
+    input [2:0] sel,
+    output reg y
+);
+
+always @(*) begin
+
+    if(sel == 3'b000)
+        y = i[0];
+    else if(sel == 3'b001)
+        y = i[1];
+    else if(sel == 3'b010)
+        y = i[2];
+    else if(sel == 3'b011)
+        y = i[3];
+    else if(sel == 3'b100)
+        y = i[4];
+    else if(sel == 3'b101)
+        y = i[5];
+    else if(sel == 3'b110)
+        y = i[6];
+    else
+        y = i[7];
+
+end
+
+endmodule
