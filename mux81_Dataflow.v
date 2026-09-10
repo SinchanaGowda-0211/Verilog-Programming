c8c2b6f842d578431c22c7ab483ef913ea97b1ea
Sinchana H M <sinchu0211@gmail.com>
2026-09-10 23:13:33 +0530

    Create mux81_Dataflow.v

---
+++ mux81_Dataflow.v
@@ -0,0 +1,16 @@
+module mux81(
+    input i0, i1, i2, i3, i4, i5, i6, i7,
+    input s2, s1, s0,
+    output y
+);
+
+assign y = (~s2 & ~s1 & ~s0 & i0) |
+           (~s2 & ~s1 &  s0 & i1) |
+           (~s2 &  s1 & ~s0 & i2) |
+           (~s2 &  s1 &  s0 & i3) |
+           ( s2 & ~s1 & ~s0 & i4) |
+           ( s2 & ~s1 &  s0 & i5) |
+           ( s2 &  s1 & ~s0 & i6) |
+           ( s2 &  s1 &  s0 & i7);
+
+endmodule
